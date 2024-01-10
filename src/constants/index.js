4e09f7cab97df0f9e862fcbfefcb025fa4713fe0
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-01-10 16:31:18 +1000

    created constant index.js file

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    href: "#about",
+    label: "About",
+  },
+  {
+    href: "#projects",
+    label: "Projects",
+  },
+  {
+    href: "#contact",
+    label: "Contact",
+  },
+];
