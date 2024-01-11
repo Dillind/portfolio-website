362b66a9d1813351cf8fedf9c025cf78b6e5b4bd
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-01-11 11:43:51 +1000

    incorporated components for each section of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello World!
-    </h1>
-  )
-}
+import { About, Projects, Contact, Footer } from "./sections/index.js";
+import Nav from "./components/Nav.jsx";
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <About />
+    </section>
+    <section className="padding">
+      <Projects />
+    </section>
+    <section className="padding-x py-10">
+      <Contact />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
+  </main>
+);
+
+export default App;
