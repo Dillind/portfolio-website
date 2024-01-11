90c8a6cd1dffca90094fa56cfc29b85fb3ad3353
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-01-11 11:44:22 +1000

    created index.js file to easily export images and icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,17 @@
+import arrowRight from "./arrow-right.svg";
+import hamburger from "./hamburger.svg";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+import github from "./github.svg";
+import copyright from "./copyright-sign.svg";
+import linkedin from "./linkedin.svg";
+
+export {
+  arrowRight,
+  hamburger,
+  instagram,
+  twitter,
+  github,
+  copyright,
+  linkedin,
+};
