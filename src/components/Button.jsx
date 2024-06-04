ccda2788e6a11ff883ca2de77e895fe064593a0f
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-06-04 20:45:19 +1000

    changed download link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,13 @@ const Button = ({ label, iconURL, linkTo }) => {
   return (
     <a
       href={linkTo}
-      download="/resume/resume.pdf"
+      download={linkTo}
       type="button"
       className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-primaryColor rounded-full text-white"
       target="_blank"
     >
       {label}
-      <img src={iconURL} alt="arrow right" className="ml-2  w-5 h-5" />
+      <img src={iconURL} alt="arrow right" className="ml-2 w-5 h-5" />
     </a>
   );
 };
